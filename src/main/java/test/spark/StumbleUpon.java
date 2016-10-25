7068d80a0023610ef35b30aa4fee2e147efcb77e
vladimirus <vladimirus@gmail.com>
2016-10-25 07:24:47 +0100

    wip

--- src/main/java/test/spark/StumbleUpon.java
+++ src/main/java/test/spark/StumbleUpon.java
@@ -1,31 +1,59 @@
 package test.spark;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.ml.feature.LabeledPoint;
+import org.apache.spark.ml.linalg.Vector;
 import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.StructType;
 import scala.Function1;
 import scala.reflect.ClassTag;
 
+import java.util.Arrays;
+
+import static java.util.Arrays.stream;
 import static org.apache.spark.sql.Encoders.javaSerialization;
 
 public class StumbleUpon {
 
     public static void main(String[] args) {
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("SparkTest")
-                .master("local[*]")
-                .getOrCreate();
+//        SparkSession spark = SparkSession
+//                .builder()
+//                .appName("SparkTest")
+//                .master("local[*]")
+//                .getOrCreate();
 
-        Dataset<Row> dataset = spark.read()
-                .option("header", true)
-                .option("delimiter", "\t")
-                .csv("/home/vov/IdeaProjects/ObserverTest/src/main/resources/train.tsv");
+//        Dataset<Row> dataset = spark.read()
+//                .option("header", true)
+//                .option("delimiter", "\t")
+//                .csv("/home/vov/IdeaProjects/ObserverTest/src/main/resources/train.tsv");
 
-        dataset.createOrReplaceTempView("links");
+//        dataset.select("label").show();
+//        dataset.show();
+//        spark.stop();
+
+        SparkConf conf = new SparkConf().setAppName("SparkTest").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String[]> data = sc.textFile("/home/vov/IdeaProjects/ObserverTest/src/main/resources/train_noheader.tsv")
+                .map(line -> line.split("\t"))
+                .map(array -> stream(array).map(item -> item.replaceAll("\"", "")).toArray((String[]::new)));
+
+
+        JavaRDD<LabeledPoint> labelPoints = data.map(array -> {
+            //something
+
+            double[] features;
+            return new LabeledPoint(1.0, Vectors.dense(features));
+        });
+
+
+        System.out.println(data.first()[0]);
+
+
 
 
 //        dataset.map( r ->
@@ -36,11 +64,7 @@ public class StumbleUpon {
 //            LabeledPoint(label, Vectors.dense(features))
 //        }
 
-        dataset.select("label").show();
 
-        dataset.show();
-
-        spark.stop();
     }
 
 }
